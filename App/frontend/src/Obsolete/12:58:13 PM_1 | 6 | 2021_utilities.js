616909da26452e8540710271acb957add52c890e
soberservicesguy <arsalan.ahmad.ishaq@gmail.com>
2021-04-23 01:10:00 +0500

    worked more

--- App/frontend/src/Obsolete/12:58:13 PM_1 | 6 | 2021_utilities.js
+++
@@ -1,7 +0,0 @@
-
-const utils = {
-    baseUrl:'http://984e1ab12822.ngrok.io',
-    productsEndpoint:'',
-}
-
-export default utils
--- App/frontend/src/utilities.js
+++ App/frontend/src/utilities.js
@@ -1,8 +1,8 @@
 
 const utils = {
-    // baseUrl:'http://localhost:3001', // for development
+    baseUrl:'http://localhost:3001', // for development
     // baseUrl:'http://localhost:80', // for docker
-    baseUrl:'http://hello-world.info:80', // for Kubernetes 
+    // baseUrl:'http://hello-world.info:80', // for Kubernetes 
 
     image:require("./images/samosa.jpeg").default,
 }
